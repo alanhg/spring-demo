1d11585b5ab1f2fa4b863dc97248adac6ee387e0
alanhg <qianghe421@163.com>
2018-08-17 12:31:08 +0800

    static resource support

---
+++ src/main/java/com/example/springdemo/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UserController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String showWelcomePage(ModelMap model) {
+        System.out.println(111);
+        model.put("name", "alan");
+        return "welcome";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,4 @@
+<div class="container">
+    Welcome ${name}!! <a href="/list-todos">Click here</a> to manage your
+    todo's.
+</div>
